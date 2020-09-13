fde473139168a724841752d07e894de00b8001e8
Cooper Saye <coopersaye@gmail.com>
2020-09-13 15:56:08 -0700

    Implement equation input regex.

---
+++ Assets/Grapher/Scripts/EquationGrapher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Grapher
+{
+    public class EquationGrapher : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject pointPrefab;
+
+        private GameObject[] points = new GameObject[100];
+
+        public void GraphEquation()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Grapher/Scripts/EquationInput.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Grapher
+{
+    [RequireComponent(typeof(TMP_InputField))]
+    public class EquationInput : MonoBehaviour
+    {
+        private TMP_InputField input;
+
+        private void Start()
+        {
+            input = GetComponent<TMP_InputField>();
+        }
+
+        public void UpdateEquation()
+        {
+
+        }
+    }
+}
