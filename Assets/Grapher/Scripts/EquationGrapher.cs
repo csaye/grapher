ec7d62b2431b4461ffa5bea37521cce34dabb214
Cooper Saye <coopersaye@gmail.com>
2020-09-14 15:18:12 -0700

    Implement basic valid equation check.

--- Assets/Grapher/Scripts/EquationGrapher.cs
+++ Assets/Grapher/Scripts/EquationGrapher.cs
@@ -4,14 +4,14 @@ namespace Grapher
 {
     public class EquationGrapher : MonoBehaviour
     {
-        [Header("References")]
-        [SerializeField] private GameObject pointPrefab;
+        // [Header("References")]
+        // [SerializeField] private GameObject pointPrefab = null;
 
         private GameObject[] points = new GameObject[100];
 
-        public void GraphEquation()
+        public void GraphEquation(string equation)
         {
-            
+            Debug.Log("graphing equation " + equation);
         }
     }
 }
--- Assets/Grapher/Scripts/EquationInput.cs
+++ Assets/Grapher/Scripts/EquationInput.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace Grapher
@@ -6,6 +7,9 @@ namespace Grapher
     [RequireComponent(typeof(TMP_InputField))]
     public class EquationInput : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private EquationGrapher equationGrapher = null;
+
         private TMP_InputField input;
 
         private void Start()
@@ -15,7 +19,30 @@ namespace Grapher
 
         public void UpdateEquation()
         {
+            string equation = input.text;
 
+            if (IsValidEquation(equation)) equationGrapher.GraphEquation(equation);
+        }
+
+        private bool IsValidEquation(string equation)
+        {
+            string[] sides = equation.Split('=');
+            
+            if (sides.Length != 2) return false;
+
+            if (string.IsNullOrEmpty(sides[0])) return false;
+            if (string.IsNullOrEmpty(sides[1])) return false;
+
+            bool leftX = sides[0].Contains('x');
+            bool leftY = sides[0].Contains('y');
+            bool rightX = sides[1].Contains('x');
+            bool rightY = sides[1].Contains('y');
+
+            if (leftX && leftY) return false;
+            if (rightX && rightY) return false;
+            if (!leftX && !leftY && !rightX && !rightY) return false;
+
+            return true;
         }
     }
 }
